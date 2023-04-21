a1ba8edf3eb3297a49e435aae4a2e43c15d646ab
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-04-21 18:49:21 -0300

    added error handling

--- index.js
+++ index.js
@@ -3,18 +3,32 @@ import chalk from 'chalk';
 
 pegaArquivo('./arquivos/texto.md');
 
-function pegaArquivo(caminhoArquivo){
-    const encondig = 'utf-8'
-    fs.readFile(caminhoArquivo, encondig, (erro, texto)=>{ //metodo utilizado para trabalhar com leitura no fs
-       if (erro){
-        trataErro(erro);
-       }
-        console.log(chalk.green(texto));
-    } )
+
+async function pegaArquivo (caminhoArquivo){
+    try{
+        const encondig = 'utf-8';
+        const resposta = await fs.promises.readFile(caminhoArquivo, encondig);
+        console.log(resposta);}
+
+    catch(erro) {
+         trataErro(erro);
+    }
+
 }
+
+// function pegaArquivo(caminhoArquivo){  //versão assincrono com then
+//     const encondig = 'utf-8';
+//     fs.promises
+//      .readFile(caminhoArquivo, encondig)
+//      .then((texto)=> console.log(chalk.green(texto))) 
+//      .catch((erro)=>trataErro(erro))
+// }
+
 
 function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretorio'));
 }
+
+
 
 
