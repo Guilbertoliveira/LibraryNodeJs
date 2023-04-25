2d61f78031e6f01914501b4114d6479b68ae2af7
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-04-25 10:14:22 -0300

    code improvements

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,30 @@
 import chalk from "chalk";
+import fs from "fs";
 import pegaArquivo from "./index.js";
 
 
-const caminho = process.argv;  // process é um comando do node, argv (argumento, informação passada por linha de comando)
+const caminho = process.argv;  // process é um comando do node, argv (argumento, informado pelo usuario passará no terminal)
 
-async function processaTexto(caminho){
-    const resultado = await pegaArquivo(caminho[2]);
-    console.log(chalk.yellow('lista de links'), resultado);
+async function processaTexto(argumentos){
+
+    //metodo do fs (lstatSync(caminhoquedesejaverificar)), isFile retorna ou true ou false
+    if (fs.lstatSync(argumentos[2]).isFile()){
+        const resultado = await pegaArquivo(argumentos[2]);
+        console.log(chalk.yellow('lista de links'), resultado);       
+    }
+    else if (fs.lstatSync(argumentos[2]).isDirectory){
+        const arquivos = await fs.promises.readdir(argumentos[2]); //demonstra arquivos dentro do diretorio
+        arquivos.forEach(async arquivo => {
+            const verificacao = await pegaArquivo(`${argumentos[2]}/${arquivo}`);
+            imprimirTela(`${argumentos[2]}${arquivo}`, verificacao);
+        });
+    }
+
+}
+
+function imprimirTela(caminho, objeto){
+    console.log(chalk.yellow(`Arquivo referente ao: ${caminho}`));
+    console.log(objeto);
 }
 
 processaTexto(caminho);
